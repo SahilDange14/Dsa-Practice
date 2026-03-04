c89249a114ceedb3437f0d679d67ae10316da18a
SahilDange14 <119867452+SahilDange14@users.noreply.github.com>
2026-03-04 13:27:29 +0530

    Solve reverse string prefix

---
+++ two-pointers/3794-reverse-string.java
@@ -0,0 +1,19 @@
+//Difficulty : Easy
+//Problem: Reverse String Prefix
+
+
+class Solution {
+    public String reversePrefix(String s, int k) {
+        char[] chars = s.toCharArray();
+        int left = 0 , right = k -1 ;
+        while (left < right){
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
+            left++ ;
+            right-- ;
+        }
+        return new String(chars);
+    }
+}
