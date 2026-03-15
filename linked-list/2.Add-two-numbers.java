1bf8e33c3f874427b470f8da83e238b3458ac449
greedyraccoon <119867452+greedyraccoon@users.noreply.github.com>
2026-03-15 09:59:29 +0530

    Add function to sum two linked list numbers
    Implement a solution to add two numbers represented by linked lists.

---
+++ linked-list/2.Add-two-numbers.java
@@ -0,0 +1,27 @@
+//2.Add-two-numbers
+//Linked List medium problem 
+//tricky edge case lah used basic maths to get over it storing remainder and carrying on a digit line 21 and 22 
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy ;
+        int carry = 0 ;
+
+        while( l1 != null || l2 !=null || carry != 0){
+            int sum = carry ;
+            if(l1 != null ){
+                sum += l1.val ;
+                l1 = l1.next;
+            }
+             if(l2 != null){
+                sum += l2.val ;
+                l2 = l2.next;
+            }
+            carry = sum / 10 ;
+            curr.next = new ListNode(sum %10);
+            curr = curr.next ;
+        }
+        return dummy.next ;
+    }
+}
