eff11d066143bfe821b72938112a69974ab2fe70
greedyraccoon <119867452+greedyraccoon@users.noreply.github.com>
2026-03-27 23:24:06 +0530

    Add solution for Valid Parenthesis problem
    Implement a solution to validate parentheses using a stack.

---
+++ Stack/20.Valid Parenthesis.java
@@ -0,0 +1,20 @@
+//Stack easy questiom 
+//20.Valid Parenthesis 
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char p : s.toCharArray()) {
+            if (p == '(') {
+                stack.push(')');
+            } else if (p == '{') {
+                stack.push('}');
+            } else if (p == '[') {
+                stack.push(']');
+            } 
+            else if (stack.isEmpty() || stack.pop() != p) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
