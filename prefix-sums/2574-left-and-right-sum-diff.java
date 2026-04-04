1103f801cc7e9b710d5434083338eb4d8cb29a37
greedyraccoon <119867452+greedyraccoon@users.noreply.github.com>
2026-04-04 20:46:15 +0530

    Add solution for left and right sum difference problem

---
+++ prefix-sums/2574-left-and-right-sum-diff.java
@@ -0,0 +1,21 @@
+//2574-left-and-right-sum-diffrences
+//presum easy problem 
+
+
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        int leftSum = 0;
+        for (int i = 0; i < n; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+            answer[i] = Math.abs(leftSum - rightSum);
+            leftSum += nums[i];
+        }
+        return answer;
+    }
+}
