0f74660be3e1f7c946be7970f863328a18884478
greedyraccoon <119867452+greedyraccoon@users.noreply.github.com>
2026-04-23 20:41:11 +0530

    Create 1502.check - Arthemetic sequences.java

---
+++ Array/1502.check - Arthemetic sequences.java
@@ -0,0 +1,14 @@
+//1502.Can make arthmetic progression from sequence
+//Array easy / sorting
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int diff = arr[1] - arr[0];
+        for(int i = 2; i < arr.length; i++) {
+            if(arr[i] - arr[i-1] != diff) {
+                return false; 
+            }
+        }
+        return true;
+    }
+}
